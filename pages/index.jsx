92092006be3d419fa73d7112660e6e21da0f83cc
Jishaal Kalyan <jishaal@gmail.com>
2017-04-17 18:30:04 +1200

    Add more sections

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { config } from 'config';
-import { Wrapper } from '../components/styled/';
+import { Row, Column, Page } from 'hedron';
+import { Message, StyledLink, Text } from '../components/styled/';
+
 
 export default class Index extends React.Component {
 	render() {
 		return (
-			<Wrapper>
+			<Page>
 				<Helmet
 					title={config.siteTitle}
 					meta={[{ name: 'description', content: 'sample' }]}
 				/>
-			</Wrapper>
+
+				<Row divisions={2}>
+					<Column>
+						<Message> We off a variety of services and plans to get you ready for your driving exam. </Message>
+					</Column>
+					<Column xs={2} md={1}>
+						<h4>Section Title</h4>
+						<p>Etiam pretium libero massa, vitae lacinia nibh ultricies ut.</p>
+					</Column>
+					<Column xs={2} md={1}>
+						<h4>Section Title</h4>
+						<p>Etiam pretium libero massa, vitae lacinia nibh ultricies ut.</p>
+					</Column>
+				</Row>
+
+				<Row divisions={2}>
+					<Column>
+						<Message> Pricing plans here </Message>
+					</Column>
+					<Column xs={2} md={1}>
+						<h4>Section Title</h4>
+						<p>Etiam pretium libero massa, vitae lacinia nibh ultricies ut.</p>
+					</Column>
+					<Column xs={2} md={1}>
+						<h4>Section Title</h4>
+						<p>Etiam pretium libero massa, vitae lacinia nibh ultricies ut.</p>
+					</Column>
+				</Row>
+			</Page>
 		);
 	}
 }
